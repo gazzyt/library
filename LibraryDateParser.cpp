f609cd4f8c708146662c029d0e28c7286dae960e
gazzyt <gazzyt@live.co.uk>
2014-12-14 18:29:57 +0000

    Added skeleton dateparser class

---
+++ LibraryDateParser.cpp
@@ -0,0 +1,17 @@
+#include "LibraryDateParser.h"
+
+
+
+LibraryDateParser::LibraryDateParser()
+{
+}
+
+
+LibraryDateParser::~LibraryDateParser()
+{
+}
+
+DateTime LibraryDateParser::ParseDueDate(std::string dateString)
+{
+	return DateTime();
+}
---
+++ LibraryDateParser.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <string>
+#include "Poco\DateTime.h"
+
+using Poco::DateTime;
+
+class LibraryDateParser
+{
+public:
+	LibraryDateParser();
+~LibraryDateParser();
+
+static DateTime ParseDueDate(std::string dateString);
+};
+
